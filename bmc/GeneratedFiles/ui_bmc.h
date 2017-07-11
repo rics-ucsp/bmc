e0b442c23aeacebb3d35a7ad136c2746cae871b4
F¡l <fvilca@gmail.com>
2017-07-10 19:19:54 -0500

    adding new widget accordion

--- bmc/GeneratedFiles/ui_bmc.h
+++ bmc/GeneratedFiles/ui_bmc.h
@@ -15,6 +15,7 @@
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QComboBox>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLayout>
 #include <QtWidgets/QListWidget>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenu>
@@ -24,6 +25,7 @@
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QTableWidget>
 #include <QtWidgets/QToolBar>
+#include <QtWidgets/QToolBox>
 #include <QtWidgets/QWidget>
 #include "QVTKWidget.h"
 
@@ -41,7 +43,13 @@ public:
     QPushButton *btnOpenDCMFolder;
     QSlider *hSliderDCM;
     QSlider *verticalSlider;
+    QToolBox *toolBox;
+    QWidget *page;
+    QPushButton *pushButton_2;
+    QWidget *page_2;
     QListWidget *listWidget;
+    QWidget *page_3;
+    QWidget *page_4;
     QMenuBar *menuBar;
     QMenu *menuArchivo;
     QToolBar *mainToolBar;
@@ -51,37 +59,61 @@ public:
     {
         if (bmcClass->objectName().isEmpty())
             bmcClass->setObjectName(QStringLiteral("bmcClass"));
-        bmcClass->resize(1060, 579);
+        bmcClass->resize(1060, 795);
         actionOpen_Data_Set = new QAction(bmcClass);
         actionOpen_Data_Set->setObjectName(QStringLiteral("actionOpen_Data_Set"));
         centralWidget = new QWidget(bmcClass);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
         comboBox = new QComboBox(centralWidget);
         comboBox->setObjectName(QStringLiteral("comboBox"));
-        comboBox->setGeometry(QRect(10, 10, 69, 20));
+        comboBox->setGeometry(QRect(580, 510, 69, 20));
         pushButton = new QPushButton(centralWidget);
         pushButton->setObjectName(QStringLiteral("pushButton"));
-        pushButton->setGeometry(QRect(10, 36, 101, 23));
+        pushButton->setGeometry(QRect(480, 480, 101, 23));
         vtkRenderer = new QVTKWidget(centralWidget);
         vtkRenderer->setObjectName(QStringLiteral("vtkRenderer"));
-        vtkRenderer->setGeometry(QRect(300, 20, 751, 421));
+        vtkRenderer->setGeometry(QRect(280, 0, 771, 411));
         tableWidget = new QTableWidget(centralWidget);
         tableWidget->setObjectName(QStringLiteral("tableWidget"));
-        tableWidget->setGeometry(QRect(10, 65, 256, 192));
+        tableWidget->setGeometry(QRect(720, 420, 271, 131));
         btnOpenDCMFolder = new QPushButton(centralWidget);
         btnOpenDCMFolder->setObjectName(QStringLiteral("btnOpenDCMFolder"));
-        btnOpenDCMFolder->setGeometry(QRect(290, 450, 75, 23));
+        btnOpenDCMFolder->setGeometry(QRect(400, 500, 75, 23));
         hSliderDCM = new QSlider(centralWidget);
         hSliderDCM->setObjectName(QStringLiteral("hSliderDCM"));
-        hSliderDCM->setGeometry(QRect(380, 450, 84, 22));
+        hSliderDCM->setGeometry(QRect(300, 450, 84, 22));
         hSliderDCM->setOrientation(Qt::Horizontal);
         verticalSlider = new QSlider(centralWidget);
         verticalSlider->setObjectName(QStringLiteral("verticalSlider"));
-        verticalSlider->setGeometry(QRect(272, 10, 22, 84));
+        verticalSlider->setGeometry(QRect(1010, 440, 22, 84));
         verticalSlider->setOrientation(Qt::Vertical);
-        listWidget = new QListWidget(centralWidget);
+        toolBox = new QToolBox(centralWidget);
+        toolBox->setObjectName(QStringLiteral("toolBox"));
+        toolBox->setGeometry(QRect(10, 0, 261, 511));
+        page = new QWidget();
+        page->setObjectName(QStringLiteral("page"));
+        page->setGeometry(QRect(0, 0, 261, 403));
+        pushButton_2 = new QPushButton(page);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+        pushButton_2->setGeometry(QRect(20, 0, 101, 23));
+        QIcon icon(QIcon::fromTheme(QStringLiteral("new")));
+        pushButton_2->setIcon(icon);
+        toolBox->addItem(page, QStringLiteral("1. Entrada de Imagen DICOM"));
+        page_2 = new QWidget();
+        page_2->setObjectName(QStringLiteral("page_2"));
+        page_2->setGeometry(QRect(0, 0, 261, 403));
+        listWidget = new QListWidget(page_2);
         listWidget->setObjectName(QStringLiteral("listWidget"));
-        listWidget->setGeometry(QRect(10, 270, 256, 192));
+        listWidget->setGeometry(QRect(0, 0, 256, 192));
+        toolBox->addItem(page_2, QStringLiteral("2. Visualizacion de Modelos 3D"));
+        page_3 = new QWidget();
+        page_3->setObjectName(QStringLiteral("page_3"));
+        page_3->setGeometry(QRect(0, 0, 261, 403));
+        toolBox->addItem(page_3, QStringLiteral("3. Seleccion de Muestra de Analisis"));
+        page_4 = new QWidget();
+        page_4->setObjectName(QStringLiteral("page_4"));
+        page_4->setGeometry(QRect(0, 0, 261, 403));
+        toolBox->addItem(page_4, QStringLiteral("4. Guardar y Salir"));
         bmcClass->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(bmcClass);
         menuBar->setObjectName(QStringLiteral("menuBar"));
@@ -101,6 +133,9 @@ public:
 
         retranslateUi(bmcClass);
 
+        toolBox->setCurrentIndex(0);
+
+
         QMetaObject::connectSlotsByName(bmcClass);
     } // setupUi
 
@@ -110,6 +145,11 @@ public:
         actionOpen_Data_Set->setText(QApplication::translate("bmcClass", "Cargar", Q_NULLPTR));
         pushButton->setText(QApplication::translate("bmcClass", "Render volum\303\251trico", Q_NULLPTR));
         btnOpenDCMFolder->setText(QApplication::translate("bmcClass", "Abrir", Q_NULLPTR));
+        pushButton_2->setText(QApplication::translate("bmcClass", "Abrir Imagen", Q_NULLPTR));
+        toolBox->setItemText(toolBox->indexOf(page), QApplication::translate("bmcClass", "1. Entrada de Imagen DICOM", Q_NULLPTR));
+        toolBox->setItemText(toolBox->indexOf(page_2), QApplication::translate("bmcClass", "2. Visualizacion de Modelos 3D", Q_NULLPTR));
+        toolBox->setItemText(toolBox->indexOf(page_3), QApplication::translate("bmcClass", "3. Seleccion de Muestra de Analisis", Q_NULLPTR));
+        toolBox->setItemText(toolBox->indexOf(page_4), QApplication::translate("bmcClass", "4. Guardar y Salir", Q_NULLPTR));
         menuArchivo->setTitle(QApplication::translate("bmcClass", "Archivo", Q_NULLPTR));
     } // retranslateUi
 
